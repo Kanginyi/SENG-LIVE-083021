26601ce000d0f1b51300e083713e056798b282ed
Eric Yi <erickanginyi@gmail.com>
2021-10-13 13:57:44 -0400

    asd

--- phase-3/01-ruby-fundamentals/exercise/lib/cli.rb
+++ phase-3/01-ruby-fundamentals/exercise/lib/cli.rb
@@ -1,9 +1,13 @@
+PIZZALIST = []
+
 def init
   puts "Welcome to the lean, mean Pizza Machine App!"
   puts "Tell us your name: "
   user = gets.strip
   puts "Awesome, #{user.capitalize}, What would you like to do?"
   # need to invoke menu list methods
+  menu_options
+  menu_selection
   goodbye
 end 
 
@@ -16,6 +20,18 @@ end
 
 def menu_selection
   # Need to add some control flow based on users menu selection
+  selection = gets.strip
+  while selection != "exit"
+    if selection == "1"
+      create_pizza
+    elsif selection == "2"
+      created_pizzas
+    else
+      puts "Invalid option. Try again: "
+      menu_options
+      selection = gets.strip
+    end
+  end
 end 
 
 def create_pizza
@@ -26,6 +42,15 @@ def create_pizza
   puts "Describe this pizza: "
   desc = gets.strip 
 
+  pizza = {
+    name: name,
+    toppings: toppings,
+    desc: desc
+  }
+
+  LIST << pizza
+
+  pizza
   # need to return a pizza hash
 end 
 
@@ -33,6 +58,21 @@ end
 # 1. Think about a way to store and save the pizzas a user creates
 # 2. How can this collection be used to print a list of all of the stored pizzas
 
+def created_pizzas
+  # iterating through this array, to access the elements in the array
+  # so let's use an iterator method: .each
+
+  PIZZALIST.each do |pizza|
+    # where we write logic to do something with each element
+    # pizza is a hash
+    puts "Name: #{pizza[:name]}"
+    puts "Toppings: #{pizza[:toppings]}"
+    puts "Desc: #{pizza[:desc]}"
+  end
+end
+
 def goodbye
   puts "Come back when you get hungry!!!"
 end 
+
+init
